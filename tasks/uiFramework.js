1c37597c601314e8a8a4aa2cf68a5ca63ebe47bc
Carl Henderson <chuckhendo@gmail.com>
2015-03-05 23:28:07 -0500

    fix to make autoprefixer run on debug run (instead of just on watch)

--- tasks/uiFramework.js
+++ tasks/uiFramework.js
@@ -37,10 +37,9 @@ module.exports = function (grunt) {
   });
 
   //grunt.registerTask('build', ['wiredep', 'sass_injection', 'sass:dev', 'captain_hook:debug']);
-  grunt.registerTask('debug', ['wiredep', 'sass_injection', 'sass:dev', 'captain_hook:debug', 'browserSync', 'watch']);
+  grunt.registerTask('debug', ['wiredep', 'sass_injection', 'sass:dev', 'autoprefixer', 'captain_hook:debug', 'browserSync', 'watch']);
   grunt.registerTask('dev', ['sass:dev', 'autoprefixer', 'uncss:dev']);
   grunt.registerTask('release', ['concat:release', 'uglify', 'sass:release', 'autoprefixer', 'uncss:release', 'captain_hook:release']);
 
 
 };
-
