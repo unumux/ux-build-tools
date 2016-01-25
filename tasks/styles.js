f657d8618ebcaa6990bdc2e327c2932e8a8bd855
Sara Kennedy <dey14@sara-3s-js.up.corp.upc>
2016-01-25 14:08:22 -0500

    starts adding postcss to styles.js and installs a few postcss plugins as dependencies

--- tasks/styles.js
+++ tasks/styles.js
@@ -5,21 +5,28 @@ var config = require('../utils/config')();
 var errorHandler = require('../utils/errorHandler.js');
 var reload = require('../utils/browserSyncReload.js')();
 
+//postCSS plugin declarations
+var postcss = require('gulp-postcss');
+    autoprefixer = require('autoprefixer');
+    imageInliner = require('postcss-image-inliner');
+    cssnano = require('cssnano');
 
 gulp.task('styles', function() {
+    var processors =[
+      autoprefixer,
+      cssnano,
+      imageInliner
+  ];
+
     if(!config.local.scss) return; // if the scss paths aren't set, skip this step
+
     return gulp.src(config.local.scss.src)
         .pipe($.sourcemaps.init()) // start sourcemap processing
         .pipe($.sass({
             includePaths: [config.bowerPackageFolder]
         })) // compile the sass
         .on('error', errorHandler) // if there are errors during sass compile, call errorHandler
-        .pipe($.autoprefixer())
-        .pipe($.if(config.local.scss.base64 !== false, $.base64({
-            maxImageSize: 10*1024,
-            extensions: ['svg', 'png', 'jpg']
-        })))
-        .pipe($.if(config.local.scss.minify !== false, $.minifyCss({compatibility: 'ie8'})))
+        .pipe(postcss(processors))
         .pipe($.if(config.local.scss.minify !== false, $.rename({extname: ".min.css"})))
         .pipe($.sourcemaps.write('./', { sourceRoot: './' })) // start sourcemap processing
         .pipe(gulp.dest(config.local.scss.dest)) // output minified css to the output dir
